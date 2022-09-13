a6e32d462aeea2b4a3b603a1a97f82f69ddadd5a
snafus <james.walder@gmail.com>
2022-09-13 15:36:55 +0100

    Return permission denied on write attempt on existing file with EXCL set (#31)
    Co-authored-by: James Walder <james.walder@stfc.ac.uk>

--- src/XrdCeph/XrdCephPosix.cc
+++ src/XrdCeph/XrdCephPosix.cc
@@ -672,6 +672,8 @@ int ceph_posix_open(XrdOucEnv* env, const char *pathname, int flags, mode_t mode
  
   bool fileExists = (rc != -ENOENT); //Make clear what condition we are testing
 
+  logwrapper((char*)"Access Mode: %s flags&O_ACCMODE %d ", pathname, flags);
+
   if ((flags&O_ACCMODE) == O_RDONLY) {  // Access mode is READ
 
     if (fileExists) {
@@ -690,7 +692,11 @@ int ceph_posix_open(XrdOucEnv* env, const char *pathname, int flags, mode_t mode
           return rc;
         }
       } else {
-        return -EEXIST;
+        if (flags & O_EXCL) {
+          return -EACCES; // permission denied
+        } else {
+          return -EEXIST; // otherwise return just file exists
+        }
       }
     }
     // At this point, we know either the target file didn't exist, or the ceph_posix_unlink above removed it
