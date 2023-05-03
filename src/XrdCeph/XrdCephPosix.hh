86ae04cf7e946495b622fb562fd3f55499817f2b
Jo-STFC <jyothish.thomas@stfc.ac.uk>
2023-05-03 08:45:37 +0000

    duplicate struct definition

--- src/XrdCeph/XrdCephPosix.hh
+++ src/XrdCeph/XrdCephPosix.hh
@@ -94,34 +94,4 @@ DIR* ceph_posix_opendir(XrdOucEnv* env, const char *pathname);
 int ceph_posix_readdir(DIR* dirp, char *buff, int blen);
 int ceph_posix_closedir(DIR *dirp);
 
-/// small structs to store file metadata
-struct CephFile {
-  std::string name;
-  std::string pool;
-  std::string userId;
-  unsigned int nbStripes;
-  unsigned long long stripeUnit;
-  unsigned long long objectSize;
-};
-
-struct CephFileRef : CephFile {
-  int flags;
-  mode_t mode;
-  uint64_t offset;
-  // This mutex protects against parallel updates of the stats.
-  XrdSysMutex statsMutex;
-  uint64_t maxOffsetWritten;
-  uint64_t bytesAsyncWritePending;
-  uint64_t bytesWritten;
-  unsigned rdcount;
-  unsigned wrcount;
-  unsigned asyncRdStartCount;
-  unsigned asyncRdCompletionCount;
-  unsigned asyncWrStartCount;
-  unsigned asyncWrCompletionCount;
-  ::timeval lastAsyncSubmission;
-  double longestAsyncWriteTime;
-  double longestCallbackInvocation;
-};
-
 #endif // __XRD_CEPH_POSIX__
